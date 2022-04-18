cda3ee8ba6add4e67901de37bd06de67c323ba68
DanielIordan <99339166+DanielIordan@users.noreply.github.com>
2022-04-18 16:34:34 +0300

    Add files via upload

---
+++ Laboratorul nr.3/js/history.js
@@ -0,0 +1,5 @@
+function myFunction() {
+	document.getElementById("text").style.backgroundColor = "lightblue";
+	document.getElementById("textsimple").style.backgroundColor = "red";
+	document.getElementById("tex").style.backgroundColor = "violet";
+}
